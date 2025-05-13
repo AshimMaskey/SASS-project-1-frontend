3738ddfbd1c30e614e458d939906ae4c2fd6a18f
Ashim Maskey <ashimmaskey4@gmail.com>
2025-05-13 19:44:31 +0545

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import HomePage from "./pages/HomePage";
+import EditPage from "./pages/EditPage";
+import CreatePage from "./pages/CreatePage";
+import SinglePage from "./pages/SinglePage";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/edit-page" element={<EditPage />} />
+        <Route path="/create-page" element={<CreatePage />} />
+        <Route path="/" element={<SinglePage />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
