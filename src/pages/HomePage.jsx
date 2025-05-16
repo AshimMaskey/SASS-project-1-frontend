8f5f1a6154ae089c2cff91ad4e1567abd09b3e4d
Ashim Maskey <ashimmaskey4@gmail.com>
2025-05-16 21:20:34 +0545

    added useeffect for data fetching from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
+import axios from "axios";
+import Card from "../components/Card";
 
 const HomePage = () => {
+  const [books, setBooks] = React.useState(null);
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const fetchedBooks = await axios.get("http://localhost:3000/api/books");
+      setBooks(fetchedBooks.data);
+    };
+    fetchBooks();
+  }, []);
+
+  if (!books) {
+    return <div>Loading...</div>;
+  }
+  console.log(books);
   return (
     <>
       <Navbar />
+      <div className="flex gap-10 flex-wrap">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </>
   );
 };
