7783e34b9286ab0bc730f2323c938826250dfd43
Ashim Maskey <ashimmaskey4@gmail.com>
2025-05-21 20:47:42 +0545

    added handledelete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -24,30 +25,11 @@ const Navbar = () => {
           <div className="md:flex md:items-center md:gap-12">
             <div className="flex items-center gap-4">
               <div className="sm:flex sm:gap-4">
-                <a
-                  className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
-                  href="#"
-                >
-                  Login
-                </a>
-              </div>
-              <div className="block md:hidden">
-                <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                </button>
+                <Link to="/create-page">
+                  <button className="rounded-md cursor-pointer bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow">
+                    Add
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,20 +1,46 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const SinglePage = () => {
   const { id } = useParams();
-  const [book, setBook] = useState({});
+  const navigate = useNavigate();
+  const [book, setBook] = useState(null);
   const fetchBook = async () => {
     const response = await axios.get(`http://localhost:3000/api/books/${id}`);
     setBook(response.data.datas);
   };
+
   useEffect(() => {
     fetchBook();
   }, []);
-  console.log(book);
+  if (!book) {
+    return <div>Loading...</div>;
+  }
 
-  return <div>SinglePage</div>;
+  const handleDelete = async () => {
+    const response = await axios.delete(
+      `http://localhost:3000/api/books/${id}`
+    );
+    if (response.status === 200) {
+      alert("Book deleted successfully");
+      navigate("/");
+    }
+  };
+
+  return (
+    <>
+      <h1>{book.bookName}</h1>
+      <h1>{book.bookPrice}</h1>
+      <h1>{book.bookAuthor}</h1>
+      <button
+        onClick={handleDelete}
+        className="bg-red-500 py-1 px-2 text-white cursor-pointer hover:bg-red-600 rounded-lg"
+      >
+        Delete me{" "}
+      </button>
+    </>
+  );
 };
 
 export default SinglePage;
